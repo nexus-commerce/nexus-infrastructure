f9d36246650c23f80a0a42175aae0d8c5defc76f
voloshmiak <voloshenkomykhailo@gmail.com>
2025-11-29 23:30:52 +0200

    feat: add multiple Kafka topics and update docker-compose for notification and payment services

--- scripts/create-kafka-topics.sh
+++ scripts/create-kafka-topics.sh
@@ -15,4 +15,58 @@ kafka-topics \
   --partitions 3 \
   --replication-factor 1
 
+echo "Creating topic 'payment.succeeded'..."
+kafka-topics \
+  --create \
+  --if-not-exists \
+  --bootstrap-server $BOOTSTRAP_SERVER \
+  --topic payment.succeeded \
+  --partitions 3 \
+  --replication-factor 1
+
+echo "Creating topic 'payment.failed'..."
+kafka-topics \
+  --create \
+  --if-not-exists \
+  --bootstrap-server $BOOTSTRAP_SERVER \
+  --topic payment.failed \
+  --partitions 3 \
+  --replication-factor 1
+
+echo "Creating topic 'stock.reserved'..."
+kafka-topics \
+  --create \
+  --if-not-exists \
+  --bootstrap-server $BOOTSTRAP_SERVER \
+  --topic stock.reserved \
+  --partitions 3 \
+  --replication-factor 1
+
+echo "Creating topic 'stock.reservation.failed'..."
+kafka-topics \
+  --create \
+  --if-not-exists \
+  --bootstrap-server $BOOTSTRAP_SERVER \
+  --topic stock.reservation.failed \
+  --partitions 3 \
+  --replication-factor 1
+
+echo "Creating topic 'orders.created'..."
+kafka-topics \
+  --create \
+  --if-not-exists \
+  --bootstrap-server $BOOTSTRAP_SERVER \
+  --topic orders.created \
+  --partitions 3 \
+  --replication-factor 1
+
+echo "Creating topic 'orders.confirmed'..."
+kafka-topics \
+  --create \
+  --if-not-exists \
+  --bootstrap-server $BOOTSTRAP_SERVER \
+  --topic orders.confirmed \
+  --partitions 3 \
+  --replication-factor 1
+
 echo "All topics created successfully."
